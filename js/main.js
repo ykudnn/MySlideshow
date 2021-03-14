f07296831a8cb92a135470ab2e3fb8abdb255199
ykudnn <ykudnyksb@gmail.com>
2021-03-14 18:25:25 +0900

    add function

--- js/main.js
+++ js/main.js
@@ -35,4 +35,45 @@
     li.appendChild(img);
     document.querySelector('.thumbnails').appendChild(li);
   });
+
+  const next = document.getElementById('next');
+  next.addEventListener('click', () => {
+    let target = currentIndex + 1;
+    if (target === images.length) {
+      target = 0;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
+
+  const prev = document.getElementById('prev');
+  prev.addEventListener('click', () => {
+    let target = currentIndex - 1;
+    if (target < 0) {
+      target = images.length - 1;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
+
+  let timeoutId;
+
+  function playSlideshow() {
+    timeoutId = setTimeout(() => {
+      next.click();
+      playSlideshow();
+    }, 1000);
+  }
+
+  let isPlaying = false;
+
+  const play = document.getElementById('play');
+  play.addEventListener('click', () => {
+    if (isPlaying === false) {
+      playSlideshow();
+      play.textContent = 'Pause';
+    } else {
+      clearTimeout(timeoutId);
+      play.textContent = 'Play';
+    }
+    isPlaying = !isPlaying;
+  });
 }
